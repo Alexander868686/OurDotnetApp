f8663e0699cc3fed62dce21294474d827af58e54
Alex868686 <alexsullivan86@gmail.com>
2023-02-09 18:09:44 -0600

    Done with explorer mode.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OurDotnetApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World, my name is Alex Sullivan");
+    }
+  }
+}
